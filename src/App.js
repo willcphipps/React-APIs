68402da380df50d4149f29870d2aa7e6e97d3b54
William Phipps <willcphipps@gmail.com>
2020-05-12 16:27:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'nes.css/css/nes.min.css'
+
 import './App.css';
 
 function App() {
+    const [pokemon, setPokemon] = useState([]);
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+    }, []);
+  const showPoke = (e) => {
+    show === false ? setShow(true) : setShow(false);
+  }
+      
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <button 
+        className="m-3 btn btn-outline-dark "
+        onClick={(e)=> showPoke(e)}
+      >
+        Fetch Pokemon
+      </button>
+      <div>{
+        show === true ? 
+            pokemon.length > 0 && pokemon.map((poke, index)=>{
+                return (<div className="m-1" key={index}>{poke.name}</div>)
+            })
+          :
+          <div className="m-5">
+            <i className="nes-pokeball"></i>
+          </div>
+        }
+          
+      </div>
+        </div>
+    );
 }
+
 
 export default App;
